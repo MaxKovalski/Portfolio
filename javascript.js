7eb53715dc43d340416a6aca968ce12429da61b7
MaximKovalski <nobesko@gmail.com>
2023-04-24 00:04:40 +0300

    First Commit

---
+++ javascript.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  var typed1 = new Typed(".change-text", {
+    strings: ["Full Stack Developer", "System-Administrator"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  //   var typed2 = new Typed(".typing-2", {
+  //     strings: ["JavaScript", "React.js", "Node.js", "Express.js", "MongoDB"],
+  //     typeSpeed: 100,
+  //     backSpeed: 60,
+  //     loop: true,
+  //   });
+});
