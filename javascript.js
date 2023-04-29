78c92f76572492385d0ec842f5d114bd600878f1
MaximKovalski <nobesko@gmail.com>
2023-04-29 22:31:13 +0300

    HomeP-finish

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
 $(document).ready(function () {
   var typed1 = new Typed(".change-text", {
     strings: ["Full Stack Developer", "System-Administrator"],
@@ -5,7 +11,12 @@ $(document).ready(function () {
     backSpeed: 60,
     loop: true,
   });
-
+  document.querySelectorAll(".navbar a").forEach((n) =>
+    n.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navbar.classList.remove("active");
+    })
+  );
   //   var typed2 = new Typed(".typing-2", {
   //     strings: ["JavaScript", "React.js", "Node.js", "Express.js", "MongoDB"],
   //     typeSpeed: 100,
