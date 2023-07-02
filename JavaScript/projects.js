032215fc7480f9e8de24c98df8ed53230f811bc8
MaximKovalski <nobesko@gmail.com>
2023-07-03 00:20:32 +0300

    add2project/animFix

--- JavaScript/projects.js
+++ JavaScript/projects.js
@@ -1,17 +1,53 @@
+const borderElem = document.querySelectorAll(".border-elem");
+// PROJECT - 1
 const modalBtn = document.getElementById("modalBtn");
 const modalX = document.getElementById("modalX");
-const borderElem = document.querySelectorAll(".border-elem");
+const modal = document.getElementById("modal");
 
 modalBtn.addEventListener("click", () => {
-  document.getElementById("modal").style.display = "block";
-  borderElem.forEach((elem) => {
-    elem.style.border = "none";
-  });
+  modal.style.animation = "fadeIn 0.5s forwards";
+  modal.style.display = "flex";
+  if ((modal.style.display = "flex")) {
+    modal2.style.display = "none";
+  }
+  for (let i = 0; i < 2; i++) {
+    borderElem[i].style.border = "none";
+  }
 });
 
 modalX.addEventListener("click", () => {
-  document.getElementById("modal").style.display = "none";
-  borderElem.forEach((elem) => {
-    elem.style.border = "2px solid #393e46";
-  });
+  modal.style.animation = "fadeOut 0.5s forwards";
+  for (let i = 0; i < 2; i++) {
+    borderElem[i].style.border = "2px solid #393e46";
+  }
+
+  setTimeout(() => {
+    modal.style.display = "none";
+  }, 500);
+});
+
+// PROJECT - 2
+const modalBtn2 = document.getElementById("modalBtn2");
+const modalX2 = document.getElementById("modalX2");
+const modal2 = document.getElementById("modal2");
+modalBtn2.addEventListener("click", () => {
+  modal2.style.animation = "fadeIn 0.5s forwards";
+  modal2.style.display = "flex";
+  if ((modal2.style.display = "flex")) {
+    modal.style.display = "none";
+  }
+  for (let i = 1; i < 2; i++) {
+    borderElem[i].style.border = "none";
+  }
+});
+
+modalX2.addEventListener("click", () => {
+  modal2.style.animation = "fadeOut 0.5s forwards";
+  for (let i = 1; i < 2; i++) {
+    borderElem[i].style.border = "2px solid #393e46";
+  }
+
+  setTimeout(() => {
+    modal2.style.display = "none";
+  }, 500);
 });
