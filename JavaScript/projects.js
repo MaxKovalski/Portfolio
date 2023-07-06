d1b44af0db0dd7b04efc68a493f0a0bfa88b32c7
MaximKovalski <nobesko@gmail.com>
2023-07-07 00:54:41 +0300

    fix-project

--- JavaScript/projects.js
+++ JavaScript/projects.js
@@ -51,3 +51,29 @@ modalX2.addEventListener("click", () => {
     modal2.style.display = "none";
   }, 500);
 });
+
+// PROJECT  - 3
+const modalBtn3 = document.getElementById("modalBtn3");
+const modalX3 = document.getElementById("modalX3");
+const modal3 = document.getElementById("modal3");
+modalBtn3.addEventListener("click", () => {
+  modal3.style.animation = "fadeIn 0.5s forwards";
+  modal3.style.display = "flex";
+  if ((modal3.style.display = "flex")) {
+    modal.style.display = "none";
+  }
+  for (let i = 2; i < 3; i++) {
+    borderElem[i].style.border = "none";
+  }
+});
+
+modalX3.addEventListener("click", () => {
+  modal3.style.animation = "fadeOut 0.5s forwards";
+  for (let i = 2; i < 3; i++) {
+    borderElem[i].style.border = "2px solid #393e46";
+  }
+
+  setTimeout(() => {
+    modal3.style.display = "none";
+  }, 500);
+});
